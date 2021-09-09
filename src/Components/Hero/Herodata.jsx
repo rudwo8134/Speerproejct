5f88b2d318b2cca5692de90ac68e4454e80dfaaf
Eric Shin <ericshin@Erics-MacBook-Pro.local>
2021-09-09 14:31:47 -0400

    finish the image-slider

---
+++ src/Components/Hero/Herodata.jsx
@@ -0,0 +1,29 @@
+import image1 from '../../Assets/image1.png'
+import image2 from '../../Assets/image2.jpg'
+import image3 from '../../Assets/image3.png'
+
+const Data = [
+  {
+    alt: 'Hero image 1',
+    Header: 'INTERACTIVE CONCERT EXPERIENCE',
+    subHeader:
+      'Experience your favourite artists like never before and from the comfort of your own home.',
+    img: image1,
+  },
+  {
+    alt: 'Hero image 2',
+    Header: 'Lorem ipsum dolor',
+    subHeader:
+      'Esit amet consectetur adipisicing elit. Eum reiciendis eos a possimus quia quasi impedit eaque fugit',
+    img: image2,
+  },
+  {
+    alt: 'Hero image 3',
+    Header: ' fugiat molestiae veritatis omnis',
+    subHeader:
+      'perspiciatis voluptates dolorum neque officia saepe facere hic!',
+    img: image3,
+  },
+];
+
+export default Data
---
+++ src/Staticcss.jsx
@@ -0,0 +1,15 @@
+const staticcss = {
+  color:{
+    // hero
+    herotextcolor: "#ffffff"
+  },
+  fontSize:{
+    // hero
+    HeroMain: "65px",
+    HeroSub: "32px",
+    Herobutton:"23px"
+
+  }
+}
+
+export default staticcss
