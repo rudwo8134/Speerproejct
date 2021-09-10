fa25ab73d5973857ff9f3809b2bfef9696a209da
Eric Shin <ericshin@Erics-MacBook-Pro.local>
2021-09-09 20:53:45 -0400

    finish the hero section and nav bar

---
+++ src/Components/Layout/Navdata.jsx
@@ -0,0 +1,16 @@
+const NavData = [
+  {
+    name: 'about',
+    Link: '/about',
+  },
+  {
+    name: 'Red',
+    Link: '/red',
+  },
+  {
+    name: 'Yellow',
+    Link: '/yellow',
+  },
+];
+
+export default NavData;
