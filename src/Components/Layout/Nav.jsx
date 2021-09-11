47329342a417f3007238572d4521d8ccbdb027cf
Eric Shin <ericshin@Erics-MacBook-Pro.local>
2021-09-10 23:18:24 -0400

    find the error

--- src/Components/Layout/Nav.jsx
+++ src/Components/Layout/Nav.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styled,{keyframes} from 'styled-components'
 import { GiHamburgerMenu, GiCancel } from 'react-icons/gi';
-import { useState } from 'react/cjs/react.development'
 import NavData from './Navdata';
 import { Link } from 'react-router-dom';
 
